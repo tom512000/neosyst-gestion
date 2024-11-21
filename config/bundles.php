3ca015bbbb563e0687832e752457157d2fdc4e4c
Tom Sikora <tom.sikora03@gmail.com>
2024-11-21 15:46:40 +0100

    build: add factory and fixtures bundle and scripts

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
