6e0562d5e921df78a526ace061c69ad868542bb4
Tom Sikora <tom.sikora03@gmail.com>
2024-11-21 16:03:57 +0100

    feat: add fixtures

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -8,10 +8,26 @@ use Doctrine\Persistence\ObjectManager;
 
 class ArticleFixtures extends Fixture
 {
+    /**
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager): void
     {
         $excelService = new ExcelService();
+        $filespath = [
+            "public/upload/articles.xlsx",
+            "public/upload/articles2.xlsx",
+            "public/upload/articles3.xlsx"
+        ];
 
+        foreach ($filespath as $filepath) {
+            $spreadsheet = $excelService->readFile($filepath);
+            $data = $excelService->createArticles($spreadsheet);
+
+            foreach ($data as $article) {
+                $manager->persist($article);
+            }
+        }
 
         $manager->flush();
     }
