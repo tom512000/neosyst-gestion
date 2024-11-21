dd3827830866affc172ce1c3da48efbae3bf316f
Tom Sikora <tom.sikora03@gmail.com>
2024-11-21 16:26:17 +0100

    build: add pagination bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
