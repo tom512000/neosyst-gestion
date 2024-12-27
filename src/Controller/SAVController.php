28d70f41bda1d699b076194b5fff16402631f352
tom512000 <tom.sikora03@gmail.com>
2024-12-27 21:26:18 +0100

    feat: edit menu link

--- src/Controller/SAVController.php
+++ src/Controller/SAVController.php
@@ -11,71 +11,71 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/s/a/v')]
+#[Route('/sav')]
 final class SAVController extends AbstractController
 {
-    #[Route(name: 'app_s_a_v_index', methods: ['GET'])]
-    public function index(SAVRepository $sAVRepository): Response
+    #[Route(name: 'app_sav_index', methods: ['GET'])]
+    public function index(SAVRepository $savRepository): Response
     {
         return $this->render('sav/index.html.twig', [
-            's_a_vs' => $sAVRepository->findAll(),
+            'savs' => $savRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_s_a_v_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_sav_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $sAV = new SAV();
-        $form = $this->createForm(SAVType::class, $sAV);
+        $sav = new SAV();
+        $form = $this->createForm(SAVType::class, $sav);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($sAV);
+            $entityManager->persist($sav);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_s_a_v_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_sav_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('sav/new.html.twig', [
-            's_a_v' => $sAV,
+            'sav' => $sav,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_s_a_v_show', methods: ['GET'])]
-    public function show(SAV $sAV): Response
+    #[Route('/{id}', name: 'app_sav_show', methods: ['GET'])]
+    public function show(SAV $sav): Response
     {
         return $this->render('sav/show.html.twig', [
-            's_a_v' => $sAV,
+            'sav' => $sav,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_s_a_v_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, SAV $sAV, EntityManagerInterface $entityManager): Response
+    #[Route('/{id}/edit', name: 'app_sav_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, SAV $sav, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(SAVType::class, $sAV);
+        $form = $this->createForm(SAVType::class, $sav);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_s_a_v_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_sav_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('sav/edit.html.twig', [
-            's_a_v' => $sAV,
+            'sav' => $sav,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_s_a_v_delete', methods: ['POST'])]
-    public function delete(Request $request, SAV $sAV, EntityManagerInterface $entityManager): Response
+    #[Route('/{id}', name: 'app_sav_delete', methods: ['POST'])]
+    public function delete(Request $request, SAV $sav, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$sAV->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($sAV);
+        if ($this->isCsrfTokenValid('delete'.$sav->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($sav);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_s_a_v_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_sav_index', [], Response::HTTP_SEE_OTHER);
     }
 }
