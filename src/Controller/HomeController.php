fd818d0d8faefdcee70f35dee8d6f7cd19847c67
tom512000 <tom.sikora03@gmail.com>
2024-12-27 02:02:10 +0100

    feat: edit header

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use DateTime;
+use IntlDateFormatter;
 
 #[Route('/')]
 class HomeController extends AbstractController
@@ -12,6 +14,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $date = new DateTime();
+
+        $formatter = new IntlDateFormatter(
+            'fr_FR',
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::NONE,
+        );
+
+        return $this->render('home/index.html.twig', [
+            'currentDate' => $formatter->format($date),
+        ]);
     }
 }
