c1d3cb0a674cd9322b5e7acfeb3a81eb0cbed877
tom512000 <tom.sikora03@gmail.com>
2025-01-02 00:17:38 +0100

    feat: find all clients, savs and articles in home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\ClientRepository;
+use App\Repository\SAVRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,18 +15,24 @@ use IntlDateFormatter;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ClientRepository $clientRepository, SAVRepository $savRepository, ArticleRepository $articleRepository): Response
     {
         $date = new DateTime();
-
         $formatter = new IntlDateFormatter(
             'fr_FR',
             IntlDateFormatter::FULL,
             IntlDateFormatter::NONE,
         );
 
+        $totalClients = count($clientRepository->findAll());
+        $totalSAVs = count($savRepository->findAll());
+        $totalArticles = count($articleRepository->findAll());
+
         return $this->render('home/index.html.twig', [
             'currentDate' => $formatter->format($date),
+            'totalClients' => $totalClients,
+            'totalSAVs' => $totalSAVs,
+            'totalArticles' => $totalArticles,
         ]);
     }
 }
