0b916bd3e4bad4fa7b394568de65f6c7c79d5bdf
tom512000 <tom.sikora03@gmail.com>
2025-01-05 00:14:41 +0100

    feat: easy admin bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use App\Entity\Client;
+use App\Entity\SAV;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()->setTitle('Neosyst Gestion');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::section('Gestion des données');
+        yield MenuItem::linkToCrud('Clients', 'fa fa-user', Client::class);
+        yield MenuItem::linkToCrud('SAVs', 'fa fa-user', SAV::class);
+        yield MenuItem::linkToCrud('Articles', 'fa fa-user', Article::class);
+        yield MenuItem::linkToRoute('Retour au site', 'fas fa-home', 'home');
+    }
+}
